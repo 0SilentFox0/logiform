a8c68de8c148328448b25db5fa33531b25295be2
igor88998 <igor08082003@gmail.com>
2024-08-28 05:51:28 +0300

    update

--- src/components/PodcastSection/PodcastSection.jsx
+++ src/components/PodcastSection/PodcastSection.jsx
@@ -9,15 +9,19 @@ import MUSIC from '../../assets/episodeImages/music.png'
 import GIRL from '../../assets/episodeImages/girl.png'
 
 import PLAY from '../../assets/episodeImages/play.svg'
+import { Fragment } from 'react'
 
 
 
 function PodcastSection() {
 
     const episodes = [
-        { id: 1, title: 'No bureaucracy', description: 'Lorem ipsum', duration: '28:01', img: HUMAN },
-        { id: 2, title: 'No bureaucracy', description: 'Lorem ipsum', duration: '28:01', img: DUCK },
-        { id: 3, title: 'No bureaucracy', description: 'Lorem ipsum', duration: '28:01', img: GOD },
+        { id: 1, title: 'No bureaucracy', description: 'Lorem ipsum', duration: '28:01', img: HUMAN, link: '/episode/1' },
+        { id: 2, title: 'No bureaucracy', description: 'Lorem ipsum', duration: '28:01', img: DUCK, link: '/episode/1' },
+        { id: 3, title: 'No bureaucracy', description: 'Lorem ipsum', duration: '28:01', img: GOD, link: '/episode/1' },
+        { id: 4, title: 'No bureaucracy', description: 'Lorem ipsum', duration: '28:01', img: DUCK, link: '/episode/1' },
+        { id: 5, title: 'No bureaucracy', description: 'Lorem ipsum', duration: '28:01', img: GOD, link: '/episode/1' },
+        { id: 6, title: 'No bureaucracy', description: 'Lorem ipsum', duration: '28:01', img: GOD, link: '/episode/1' },
         // Add more episodes as needed
     ];
 
@@ -30,33 +34,33 @@ function PodcastSection() {
                 <div className={styles.episodeList}>
                     <div className={styles.episodeContainer}>
                         {episodes.map((episode, index) => (
-                            <>
-                                <div key={episode.id} className={styles.episode}>
-                                    <div className={styles.episodeLeft}>
-                                        <div className={styles.index}>0{index + 1}/</div>
-                                        <img src={episode.img} alt={episode.title} className={styles.episodeImage} />
-                                    </div>
-                                    <div className={styles.episodeRight}>
-                                        <div className={styles.episodeText}>
-                                            <h3>{episode.title}</h3>
-                                            <p>{episode.description}</p>
+                            <Fragment key={episode.id}>
+                                <a href={episode.link} className={styles.episodeLink}>
+                                    <div key={episode.index} className={styles.episode}>
+                                        <div className={styles.episodeLeft}>
+                                            <div className={styles.index}>0{index + 1}/</div>
+                                            <img src={episode.img} alt={episode.title} className={styles.episodeImage} />
                                         </div>
-                                        <div className={styles.episodeInfo}>
-                                            <h4>Episode 12</h4>
-                                            <div className={styles.durationContainer}>
-                                                <img src={MUSIC} alt='music' />
-                                                <p className={styles.duration}>{episode.duration}</p>
+                                        <div className={styles.episodeRight}>
+                                            <div className={styles.episodeText}>
+                                                <h3>{episode.title}</h3>
+                                                <p>{episode.description}</p>
+                                            </div>
+                                            <div className={styles.episodeInfo}>
+                                                <h4>Episode 12</h4>
+                                                <div className={styles.durationContainer}>
+                                                    <img src={MUSIC} alt='music' />
+                                                    <p className={styles.duration}>{episode.duration}</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </a>
                                 <div className={styles.line} />
-                            </>
+                            </Fragment>
                         ))}
                     </div>
-                    <div className={styles.scrollContainer}>
-                        <div className={styles.scroll} />
-                    </div>
+                    <div className={styles.scrollContainer} />
                 </div>
                 <div className={styles.podcastImageContainer}>
 
@@ -74,7 +78,7 @@ function PodcastSection() {
                         </svg>
                     </div>
 
-                        <img src={GIRL} alt="Podcast" className={styles.podcastImage} />
+                    <img src={GIRL} alt="Podcast" className={styles.podcastImage} />
                 </div>
             </div>
         </section>
