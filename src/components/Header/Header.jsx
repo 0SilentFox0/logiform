3a8d6c75978c4eab46e378a1603114b9af85e81e
igor88998 <igor08082003@gmail.com>
2024-08-31 16:50:41 +0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,18 @@ import styles from './Header.module.css'
 import logoImage from '../../assets/logo.svg'
 import Dropdown from './Dropdown/Dropdown'
 import HeaderUnion from './HeaderUnion/HeaderUnion'
+import { useState } from 'react'
+import { IoMdClose } from 'react-icons/io'
+import { IoMenuOutline } from 'react-icons/io5'
 
 function Header() {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <header>
             <HeaderUnion />
@@ -27,7 +37,26 @@ function Header() {
                 <div className={styles.contactButton}>
                     <button>Contact us</button>
                 </div>
+                <div className={` ${isOpen ? styles.burgerMenuOpen : styles.burgerMenu}`}>
+                    <button onClick={toggleNavbar}>{isOpen ? <IoMdClose/> : <IoMenuOutline  />}</button>
+                </div>
             </div>
+
+            {isOpen && (
+                <div className={styles.overlay}>
+                    <ul className={styles.overlayMenu}>
+                        <li><Dropdown title="Services" options={['Smart Contracts', 'Web3&BlockChain', 'Web Development', 'UI/UX design']} /></li>
+                        <li>Case studies</li>
+                        <li>About us</li>
+                        <li>Blog</li>
+                        <li><Dropdown title="Resources" options={['Option 1', 'Option 2', 'Option 3']} /></li>
+                        <li>Partner with us</li>
+                    </ul>
+                    <div className={styles.contactButtonMenu}>
+                        <button>Contact us</button>
+                    </div>
+                </div>
+            )}
 
         </header>
     )
